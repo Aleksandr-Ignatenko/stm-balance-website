b081c54dde43103465e9c21221a21e30596556f4
Aleksandr-Ignatenko <alex.ignat.1978@gmail.com>
2026-09-11 20:51:52 +0300

    Update astro.config.mjs
    Configure GitHub Pages base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: 'static'
+  output: 'static',
+  site: 'https://aleksandr-ignatenko.github.io',
+  base: '/stm-balance-website'
 });
